f3166e10e308df5c63698386426d74fb6809c9fc
Luis Mita <luis@luismita.com>
2025-07-21 12:13:53 -0400

    fix: use better servers for apt

--- setup.d/00-configure-apt.sh
+++ setup.d/00-configure-apt.sh
@@ -3,6 +3,9 @@
 set -x
 set -e
 
+# bo.archive.ubuntu.com is on Brazil and the main ISPs on the region don't have good peering to this server.
+sudo sed -i 's|bo\.archive\.ubuntu\.com|us.archive.ubuntu.com|g' /etc/apt/sources.list.d/ubuntu.sources 
+
 # Only for local testing purposes
 # I added this because the internet to teset is slow, so it was a good workaround
 
